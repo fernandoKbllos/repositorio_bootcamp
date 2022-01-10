925c66c865285c47069ecf4612467ee20104ce79
fernandoKbllos <fkabellos@gmail.com>
2022-01-10 13:55:35 +0100

    Create ejercicio.js

---
+++ js/ejercicio.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+const iPadMini = 500;
+
+const iPad = 1000;
+
+const iPadpro = 1500;
+
+let ahorros = 750;
+
+if ( ahorros > iPadpro) {
+    console.log ( "Me compro el iPad" );
+   } else if (ahorros > iPad) {
+       console.log (" Me compro el iPadPro");
+   }  else {
+    console .log (" Me conformo con el mini")
+}
