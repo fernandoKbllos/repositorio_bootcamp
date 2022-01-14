5e861f66d8f512c961861c97ba028377c8164b20
fernandoKbllos <fkabellos@gmail.com>
2022-01-14 14:01:24 +0100

    Update dia3.js

--- js/dia3.js
+++ js/dia3.js
@@ -5,4 +5,14 @@ const patiens = [true, false, true, false, false, false, true, true];
 console.log(patiens);
 
 
+function virusv2(pacientes) {
+    const pacientesCopia = [...pacientes];
+    for (let i = 0; i < pacientes.length; i++) {
+      if (pacientes[i - 1] /* ===true */ || pacientes[i + 1] /* ===true */) {
+        pacientesCopia[i] = true;
+      }
+    }
+  
+
+
 
