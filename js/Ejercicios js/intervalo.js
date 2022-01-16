7fd5d36ff068511355de45db01a975418ac853fd
fernandoKbllos <fkabellos@gmail.com>
2022-01-16 21:09:12 +0100

    Update intervalo.js

---
+++ js/Ejercicios js/intervalo.js
@@ -0,0 +1,25 @@
+"use strict"
+
+console.log("Hola intervalos");
+
+/*let secondsCounter = 1;
+setInterval(() => secondsCounter++, 100);
+
+function printCounter() {
+	const seconds = secondsCounter % 60;
+
+	const minutesCounter = Math.floor(secondsCounter / 60);
+	const minutes = minutesCounter % 60;
+
+	const hoursCounter = Math.floor(minutesCounter / 60);
+	const hours = hoursCounter % 24;
+
+	const daysCounter = Math.floor(hoursCounter / 24);
+	const days = daysCounter;
+
+	console.log(
+		`The program has been running for: ${days} days, ${hours}h, ${minutes}min and ${seconds}`
+	);
+}
+
+setInterval(printCounter, 5000);*/
