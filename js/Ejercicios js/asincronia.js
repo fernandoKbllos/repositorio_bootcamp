b39f32fa5fc6a7b52483829e5eb54799d748dc45
fernandoKbllos <fkabellos@gmail.com>
2022-01-17 13:59:20 +0100

    Create asincronia.js

---
+++ js/Ejercicios js/asincronia.js
@@ -0,0 +1,44 @@
+"use strict"
+
+/*setTimeout(() => {
+ console.log("han pasado 2 segundos");
+}, 2000)
+
+const callback  = () => {console.log("ha pasado un segundo")
+};
+setTimeout(callback, 1000);*/
+
+/*Cronómetro que cada 5s indique cuanto
+ tiempo ha pasado desde el inicio*/
+
+ /*let count = 5;
+ const callback = () => {
+     console.log(`han pasado ${count} segundos` );
+     count += 5;
+}
+ setInterval((callback), 5000);*/
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*Temporizador que a los 5s diga que se ha acabado el tiempo*/
+
